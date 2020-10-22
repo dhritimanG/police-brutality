79650f75ec75a0e9e4e68795b841d3b6be068094
Dhritiman Ganguly <ganguly.d@husky.neu.edu>
2020-10-22 18:45:37 -0400

    state is now an onject

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,14 +1,17 @@
 import * as actionTypes from "./actions"
 
-const initialState = 0
+const initialState = {
+  myNumber: 0,
+  myString: "Hello",
+}
 
 const reducer = (state = initialState, action) => {
   switch (action.type) {
     case actionTypes.ADD: {
-      return state + 1
+      return { ...state, myNumber: state.myNumber + 1 }
     }
     case actionTypes.SUBTRACT: {
-      return state - 1
+      return { ...state, myNumber: state.myNumber - 1 }
     }
   }
 }
